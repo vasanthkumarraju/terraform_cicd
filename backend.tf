59b3a52970dd88d83f6ceee745421ed070ecb431
Vasanth Kumar <vasanthkuamar135@gmail.com>
2026-02-14 17:25:28 +0530

    updated backend and workflows file

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
     storage_account_name = "tfstatestoreforsa"
-    container_name       = "tfstateblob"
+    container_name       = "tfstate"
     key                  = "dev/terraform.tfstate"
   }
 }
